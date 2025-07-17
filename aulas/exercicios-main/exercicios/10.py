0f46a948d50d3d1ce2e9f4097546a506ad0ae47d
GDotto25 <gdottobt@gmail.com>
2025-07-16 21:50:39 -0300

    Add files via upload

---
+++ aulas/exercicios-main/exercicios/10.py
@@ -0,0 +1,6 @@
+
+print("Faça um Programa que peça a temperatura em graus Celcius, transforme e mostre em graus Farenheit.")
+
+c = float(input("Insira o seu valor Celcius: "))
+f = (c*9/5)
+print("Fahrenheit é igual: ",f)
---
+++ aulas/exercicios-main/exercicios/9.py
@@ -0,0 +1,12 @@
+# Faça um Programa que peça a temperatura em graus Farenheit
+# Transforme e mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9).
+print("Faça um Programa que peça a temperatura em graus Farenheit")
+
+f = float(input('Digite o valor em Farenheit: '))
+c = (5* (f-32) / 9)
+
+print('Celcius é igual : ',c)
+
+
+
+
