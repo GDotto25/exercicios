744d46ae1fd6a44f26ac8beb30fb88c4177484eb
GDotto25 <gdottobt@gmail.com>
2025-07-17 09:35:34 -0300

    Delete exercicios directory

--- exercicios/2.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
-
-n1 = input('Digite um número:') #pedir a variavel
-print(n1) #imprimir a variavel
- 
